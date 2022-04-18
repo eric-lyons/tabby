b10baebe4b0e07bb377a5e65851c1e9f3e6b18a9
Eric Lyons <53839634+eric-lyons@users.noreply.github.com>
2022-04-18 13:55:20 -0400

    Create action_form.py

---
+++ action_form.py
@@ -0,0 +1,41 @@
+# https://github.com/looker/actions/blob/master/docs/action_api.md#action-form-endpoint
+import os
+import hmac
+from flask import Response
+import json
+
+# https://github.com/looker/actions/blob/master/docs/action_api.md#authentication
+def authenticate(request):
+    if request.method != 'POST':
+        r =  '|ERROR| Request must be POST'; print (r)
+        return Response(r, status=401, mimetype='application/json')
+
+    elif 'authorization' not in request.headers:
+        r = '|ERROR| Request does not have auth token'; print (r)
+        return Response(r, status=400, mimetype='application/json')
+
+    else:
+        expected_auth_header = 'Token token="{}"'.format(os.environ.get('header'))
+        submitted_auth = request.headers['authorization']
+        if hmac.compare_digest(expected_auth_header,submitted_auth):
+            return Response(status=200, mimetype='application/json')
+
+        else:
+            r = '|ERROR| Incorrect token'; print (r)
+            return Response(r, status=403, mimetype='application/json')
+
+
+
+def action_form(request):
+    auth = authenticate(request)
+    if auth.status_code != 200:
+        return auth
+
+    response = [
+  {"name": "filename", "label": "Subject Name", "type": "string"},
+  {"name": "email", "label": " email address", "type": "string"}
+      ]
+
+    print ('returning form json')
+    return json.dumps(response)
+    #return Response(json.dumps(response), status=200, mimetype='application/json')
